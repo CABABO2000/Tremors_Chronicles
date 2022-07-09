71d2d2cdbf3aa976e765189dfb7a20c39004caa1
CABABO2000 <babodino08@gmail.com>
2022-07-09 11:13:55 -0700

    commit

--- src/main/java/net/CABABO2000/tc/entity/custom/JrowEntity.java
+++ src/main/java/net/CABABO2000/tc/entity/custom/JrowEntity.java
@@ -1,5 +1,6 @@
 package net.CABABO2000.tc.entity.custom;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
@@ -9,6 +10,9 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.util.AirAndWaterRandomPos;
+import net.minecraft.world.entity.ai.util.HoverRandomPos;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.animal.FlyingAnimal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Zombie;
@@ -17,16 +21,22 @@ import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Contract;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import javax.annotation.Nullable;
+import java.util.EnumSet;
+import java.util.Random;
 
 public class JrowEntity extends Monster implements IAnimatable, FlyingAnimal {
     private AnimationFactory factory = new AnimationFactory(this);
 
     public JrowEntity(EntityType<? extends Monster> p_33002_, Level p_33003_) {
         super(p_33002_, p_33003_);
-        this.moveControl = new FlyingMoveControl(this, 20, false);
+        this.moveControl = new FlyingMoveControl(this, 20, true);
         this.setPathfindingMalus(BlockPathTypes.DANGER_FIRE, 2.0F);
         this.setPathfindingMalus(BlockPathTypes.WATER, -2.0F);
         this.setPathfindingMalus(BlockPathTypes.WATER_BORDER, -2.0F);
@@ -39,7 +49,7 @@ public class JrowEntity extends Monster implements IAnimatable, FlyingAnimal {
                 .add(Attributes.ATTACK_DAMAGE, 5.0f)
                 .add(Attributes.ATTACK_SPEED, 2.0f)
                 .add(Attributes.ATTACK_KNOCKBACK, 0.0f)
-                .add(Attributes.MOVEMENT_SPEED, 0.25f)
+                .add(Attributes.MOVEMENT_SPEED, 0.4f)
                 .add(Attributes.FOLLOW_RANGE, 25f)
                 .add(Attributes.FLYING_SPEED, 0.5f).build();
     }
@@ -50,7 +60,7 @@ public class JrowEntity extends Monster implements IAnimatable, FlyingAnimal {
         this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(4, new FollowMobGoal(this, 1f, 3, 10));
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.3f, true));
-        this.goalSelector.addGoal(4, new WaterAvoidingRandomFlyingGoal(this, 5));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomFlyingGoal(this, 1.3f));
         this.goalSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Zombie.class, true));
         this.goalSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, true));
         this.goalSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Villager.class, true));
